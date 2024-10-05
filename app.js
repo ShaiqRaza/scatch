c32f791d36963f2b71da72f076b58f6c3ce49352
ShaiqRaza <shaiqraza808y@gmail.com>
2024-10-05 17:58:34 +0500

    server setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+app.use(express.urlencoded({extende: true}));
+app.use(express.static('./public'));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!");
+    console.log("Program is running...");
+})
