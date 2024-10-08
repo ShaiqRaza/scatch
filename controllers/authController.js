129ac0cea8b06bc5d9ebf80fc498b55348d9745c
ShaiqRaza <shaiqraza808y@gmail.com>
2024-10-09 02:42:35 +0500

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,45 +4,60 @@ const jwt = require('jsonwebtoken')
 
 let createUser = async (req, res)=>{
     let {fullname, email, password} = req.body;
+    
+    if( ! (fullname && email && password))
+        return res.status(400).send("Body parameters are not Complete")
 
-    let user = await userModel.findOne({email});
-    if(user) 
-        return res.status(402).send("You already have an account plaease login.");
+    let existingUser = await userModel.findOne({email});
+    if(existingUser) 
+        return res.status(400).send("You already have an account plaease login.");
 
-    bcrypt.genSalt(10, (err, salt)=>{
-        bcrypt.hash(password, salt, async (err, hash)=>{
-            if(err)
-                res.send(err.message);
-            let createdUser = await userModel.create({
-                fullname,
-                email,
-                password:hash
-            })
-            let token = jwt.sign({email: createdUser.email, _id: createdUser._id}, process.env.JWT_KEY);
-            res.cookie("token", token);
-            res.send("Account created")
-        } )
-    })
+    try
+    {
+        let salt = await bcrypt.genSalt(10);
+        let hash = await bcrypt.hash(password, salt);
+        let createdUser = await userModel.create({
+            fullname,
+            email,
+            password:hash
+        })
+        let token = jwt.sign({email: createdUser.email, _id: createdUser._id}, process.env.JWT_KEY);
+        res.cookie("token", token);
+        res.send("Account created")
+    } 
+    catch(err) 
+    {
+        res.status(500).send(err.message);
+    }
 }
 
 module.exports.createUser = createUser;
 
 let loginUser = async (req, res)=>{
     let {email, password} = req.body;
-    let user = await userModel.findOne({email});
-    if(user){
-        bcrypt.compare(password, user.password, (err, result)=>{
-            if(err)
-                res.send(err.message);
-            if(!result)
-                return res.status(403).send("password or email is incorrect");
-            let token = jwt.sign({email: user.email, _id: user._id}, process.env.JWT_KEY);
-            res.cookie("token", token);
-            res.send("Account login")
-        })
+    if( ! (email && password))
+        return res.status(400).send("Body parameters are not Complete")
+
+    try {
+        let existingUser = await userModel.findOne({email});
+
+        if(existingUser){
+            bcrypt.compare(password, user.password, (err, result)=>{
+                if(err)
+                    return res.status(500).send(err.message);
+                else if(!result)
+                    return res.status(400).send("password or email is incorrect");
+                let token = jwt.sign({email: existingUser.email, _id: existingUser._id}, process.env.JWT_KEY);
+                res.cookie("token", token);
+                res.send("Account login")
+            })
+        }
+        else
+            res.status(400).send("password or email is incorrect");
     }
-    else
-        res.status(402).send("password or email is incorrect");
+    catch (err) {
+        res.status(500).send(err.message);
+    }
 }
 
 module.exports.loginUser = loginUser;
