a03615b5af6d6403a6f158f9dd9541ee96f179fb
ShaiqRaza <shaiqraza808y@gmail.com>
2024-10-20 23:14:08 +0500

    create product popup update

--- public/scripts/panelScript.js
+++ public/scripts/panelScript.js
@@ -1,4 +1,5 @@
 let addProductPopup = document.querySelector('#addProductPopup');
+let addProductPopup_reverse = document.querySelector('#addProductPopup_reverse');
 let addProduct = document.querySelector('#addProduct');
 let addProductClick = true;
 addProduct.addEventListener('click', ()=>{
@@ -6,6 +7,14 @@ addProduct.addEventListener('click', ()=>{
         addProductPopup.style.display = "block";
     else
         addProductPopup.style.display = "none";
+
+    addProductClick = !addProductClick;
+});
+addProduct.addEventListener('click', ()=>{
+    if(!addProductClick) 
+        addProductPopup_reverse.style.display = "block";
+    else
+    addProductPopup_reverse.style.display = "none";
 
     addProductClick = !addProductClick;
 });
