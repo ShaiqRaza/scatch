8d007173eb6911f7a1f8cab9ccf983cbc96d68e7
ShaiqRaza <shaiqraza808y@gmail.com>
2024-11-07 11:07:15 +0500

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const MongoStore = require('connect-mongo');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('./public'));
+app.use(express.static( path.join(__dirname, 'public') ));
 app.use(cookieParser());
 
 app.use(
